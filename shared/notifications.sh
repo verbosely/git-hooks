064a9bbe5f32958d1d2f4ce37bfe0998ccc9acaf
Zachary Flohr <zachary.flohr@case.edu>
2025-04-08 18:21:24 -0400

    Define a function for printing git hook starts and stops

--- shared/notifications.sh
+++ shared/notifications.sh
@@ -66,9 +66,20 @@ terminate() {
     case "${FUNCNAME[1]}" in
         'check_binaries')
             error_msg="You must install the following tools "
-            error_msg+="to run this script: ${1}"
+            error_msg+="to run this script: ${1}\nAborting!"
         ;;
     esac
     print_message 1 "red" "${error_msg}"
     exit ${exit_status}
+}
+
+print_hook_lifecycle() {
+    case "${1}" in
+        'start')
+            print_message 0 "cyan" "Running $(basename "${2}")..."
+        ;;
+        'end')
+            print_message 0 "cyan" "$(basename "${2}") completed."
+        ;;
+    esac
 }
