7b741754e62ce5127f0bcff45b8a7afa081e5ab9
Zachary Flohr <zmflohr@gmail.com>
2025-05-22 04:11:14 -0400

    Combine variable declarations onto single line

--- shared/checks.sh
+++ shared/checks.sh
@@ -4,12 +4,11 @@
 . $(dirname ${BASH_SOURCE[0]})/notifications.sh
 
 check_binaries() {
-    local -a missing_binaries=()
-    local -ar NEEDED_BINARIES=($*)
+    local -a missing_binaries=() ; local -ar NEEDED_BINARIES=($*) ; local binary
     which which &> /dev/null || terminate "which"
     for binary in "${NEEDED_BINARIES[@]}"; do
         which ${binary} &> /dev/null || missing_binaries+=($binary)
     done
-    ! (( ${#missing_binaries[*]} )) || terminate "${missing_binaries[*]}"
+    ! (( ${#missing_binaries[*]} )) || terminate "${missing_binaries[@]}"
     unset -f check_binaries
 }
